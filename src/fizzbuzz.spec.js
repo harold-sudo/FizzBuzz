f46dc0e48c58034c9b45edad5d705be76f4b55a1
Harold Sejas <harold.sejas@ucb.edu.bo>
2025-08-27 20:46:56 -0400

    Feat: Test en rojo/un numero cualquiera no multiplo

--- src/fizzbuzz.spec.js
+++ src/fizzbuzz.spec.js
@@ -4,5 +4,10 @@ describe("FizzBuzz", () => {
   it("un numero no multiplo de 3 y 5", () => {
     expect(fizzbuzz("1")).toEqual("1");
   });
+
+  it("un numero cualquiera que no es multiplo de 3 y 5", () => {
+    expect(fizzbuzz("2")).toEqual("2");
+  });
+
 });
 
